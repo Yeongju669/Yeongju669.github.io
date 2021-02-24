3e2e38c4727208acc6895f781c298f0994b05afa
Yeongju669 <Yeongju669@users.noreply.github.com>
2021-02-23 22:24:16 -0500

    project 2 pt 6 draft 2

--- projects/n6/js/2/scripts.js
+++ projects/n6/js/2/scripts.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 	var winWidth = $(window).width();
 	var winHeight = $(window).height();
 	var slide = 0;
-	var colours = ['blue', 'red', 'yellow', 'purple', 'green', 'orange'];
+	var colours = [];
 	var colourIndex = 0;
 	var divider = 1/$(document).height();
 
-	if(winWidth < 600){
+	if(winWidth = 400){
 		var size = winHeight < winWidth ? winWidth : winHeight;
 		$('.flash').css({
 			width:size*2,
@@ -16,6 +16,7 @@ $(document).ready(function(){
 			marginLeft:-size
 		});
 	}
+
 	// $(document).scroll(function(){
 	// 	scrollTop = $(document).scrollTop();
 	// 	scale = 1 - (scrollTop * divider);
@@ -48,43 +49,6 @@ $(document).ready(function(){
 		$('header').show();
 		e.preventDefault();
 	});
-
-	document.addEventListener('touchmove', function(e){
-		$('.flash, .white').css({
-			top:e.pageY,
-			left:e.pageX
-		});
-		e.preventDefault();
-	}, false);
-
-	document.addEventListener('touchstart', function(e){
-		if(!$(e.target).hasClass('email')){
-			$('.flash, .white').css({
-				top:e.pageY,
-				left:e.pageX
-			});
-			$('.flash').removeClass('blue red yellow purple green orange').addClass('flashing black');
-			$('header').hide();
-			$('.info').show();
-			slide = slide >= $('.slides li').length - 1 ? slide = 0 : slide + 1;
-			$('.slides li').eq(slide).addClass('visible');
-			e.preventDefault();
-		}
-	}, false);
-
-	document.addEventListener('touchend', function(e){
-		if(!$(e.target).hasClass('email')){
-			$('.slides li').removeClass('visible');
-			colourIndex = colourIndex === 5 ? colourIndex = 0 : colourIndex + 1;
-			$('.flash').addClass(colours[colourIndex]).removeClass('flashing black');
-			$('.info').hide();
-			$('header').show();
-			e.preventDefault();
-		}
-	}, false);
-
-
-
 
 
 
