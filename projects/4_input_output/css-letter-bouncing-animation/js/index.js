1e87d85f01e6fb1fae4ca18c584cc4463d5f2748
Yeongju669 <Yeongju669@users.noreply.github.com>
2021-04-23 13:03:11 -0400

    organized

---
+++ projects/4_input_output/css-letter-bouncing-animation/js/index.js
@@ -0,0 +1,38 @@
+
+
+let myText = document.getElementById("bounceTxt").innerHTML,
+    wrapText = "";
+
+for (let i=0; i<myText.length; i++) {
+     wrapText += "<em>" + myText.charAt(i) + "</em>";
+}
+
+document.getElementById("bounceTxt").innerHTML = wrapText;
+
+let myLetters = document.getElementsByTagName("em"),
+    j = 0;
+
+
+    
+function applyBounce() {
+     setTimeout(function() {
+          myLetters[j].className = "bounce-me";
+          j++;
+          
+          if (j < myLetters.length) {
+               applyBounce();
+          }
+     }, 250);
+}
+
+applyBounce();
+
+
+
+
+
+
+
+
+
+
